24daa15a5495cb654f07630c5069cf6189568032
nemo-nemo-bim <drakequation@naver.com>
2025-08-01 03:42:33 +0900

    💡 feat(header): 구현 - 장바구니 아이템 수 동적 표시 기능 추가
    - Header 컴포넌트에서 "0" 대신 동적 cartItemCount 표시
    - App 컴포넌트에 useState로 cartItemCount 상태 추가
    - cartItemCount를 Header로 prop 전달

--- src/advanced/App.tsx
+++ src/advanced/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import ShoppingCart from './components/cart/ShoppingCart';
 import GuideToggle from './components/guide/GuideToggle';
 import Header from './components/layout/Header';
@@ -5,9 +7,11 @@ import Layout from './components/layout/Layout';
 import OrderSummary from './components/order/OrderSummary';
 
 const App = () => {
+  const [cartItemCount, setCartItemCount] = useState(0);
+
   return (
     <>
-      <Header />
+      <Header cartItemCount={cartItemCount} />
       <GuideToggle />
       <Layout>
         <ShoppingCart />
