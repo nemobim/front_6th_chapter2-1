6b29e46603e2bd6a19d580f15dd58ea1ab0d80dd
nemo-nemo-bim <drakequation@naver.com>
2025-08-01 05:19:56 +0900

    🔌 feat: App에 CartProvider 적용
    - 전체 앱을 CartProvider로 감싸기
    - 전역 상태 관리 활성화
    - 컴포넌트 간 상태 공유 가능

--- src/advanced/App.tsx
+++ src/advanced/App.tsx
@@ -3,17 +3,18 @@ import GuideToggle from './components/guide/GuideToggle';
 import Header from './components/layout/Header';
 import Layout from './components/layout/Layout';
 import OrderSummary from './components/order/OrderSummary';
+import { CartProvider } from './hooks/CartContext';
 
 const App = () => {
   return (
-    <>
+    <CartProvider>
       <Header />
       <GuideToggle />
       <Layout>
         <ShoppingCart />
         <OrderSummary />
       </Layout>
-    </>
+    </CartProvider>
   );
 };
 
